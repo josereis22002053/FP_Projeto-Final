709cc8bc2f4a45174259cab97bc2974f1fb7e41a
unknown <madacagido@gmail.com>
2021-01-28 22:06:42 +0000

    peças e botoes imagens

--- constants.py
+++ constants.py
@@ -6,3 +6,15 @@ RED = (255, 0, 0)
 # SCREEN DIMENSIONS
 WIDTH = 1280
 HEIGHT = 720
+
+# PIECES
+ALIEN = pygame.image.load('alien.png')
+PREDATOR = pygame.image.load('predator.png')
+
+# BUTTONS
+PLAY = pygame.image.load('play.png')
+PLAY_HIGHLIGHT = pygame.image.load('play_highlight.png')
+EXIT = pygame.image.load('exit.png')
+EXIT_HIGHLIGHT = pygame.image.load('exit_highlight.png')
+MENU = pygame.image.load('menu.png')
+MENU_HIGHLIGHT = pygame.image.load('menu_highlight.png')
